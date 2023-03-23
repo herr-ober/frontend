347cc4618f35e845635554e5610c37c1d130b40d
Sven Luipold <inf21155@lehre.dhbw-stuttgart.de>
2023-03-23 10:10:51 +0100

    spike(#26): Add button

---
+++ src/app/shared/components/inputfield/inputfield.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inputfield',
+  templateUrl: './inputfield.component.html',
+  styleUrls: ['./inputfield.component.css']
+})
+export class InputfieldComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,19 +1,24 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from "@angular/forms";
+import { ButtonComponent } from './components/button/button.component';
+import { InputfieldComponent } from './components/inputfield/inputfield.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ButtonComponent,
+    InputfieldComponent
+  ],
   imports: [
     CommonModule,
     FormsModule
   ],
   exports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    ButtonComponent
   ]
 })
 export class SharedModule {
-  //Hall0
 }
