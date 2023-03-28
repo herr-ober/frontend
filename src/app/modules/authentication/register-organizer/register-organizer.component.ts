5b5434a1e8af457e390ae4b3253febfbb81a0195
Angmar3019 <github@account.vockensohn.de>
2023-03-28 23:13:02 +0200

    Add register form

--- src/app/modules/authentication/register-organizer/register-organizer.component.ts
+++ src/app/modules/authentication/register-organizer/register-organizer.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-organizer',
   templateUrl: './register-organizer.component.html',
   styleUrls: ['./register-organizer.component.css']
 })
-export class RegisterOrganizerComponent {
+export class RegisterOrganizerComponent implements OnInit {
+  visible: boolean = true;
+  message: string = '';
+
+  show(message: string) {
+    this.visible = true;
+    this.message = message;
+  }
+
+  closeAlert() {
+    this.visible = false;
+  }
+
+  
+  registerForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+  }
 
 }
