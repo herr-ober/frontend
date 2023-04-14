37388bdf48f2139b4f57f2da715ed67d6a32aea9
Jannis636 <32140646+Jannis636@users.noreply.github.com>
2023-04-14 11:17:09 +0200

    fix(#73): Remove hardcoded port

--- src/app/core/services/staff.service.ts
+++ src/app/core/services/staff.service.ts
@@ -1,14 +1,12 @@
 import {Injectable} from "@angular/core";
 import {ApiService} from "./api.service";
 import {IEvent} from "../../models/IEvent";
-import {HttpClient} from "@angular/common/http";
 import {IStaff} from "../../models/IStaff";
-import {firstValueFrom} from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class StaffService {
 
-    constructor(private apiService: ApiService, private httpClient: HttpClient) {}
+    constructor(private apiService: ApiService) {}
 
     async addStaff(event: IEvent, name: string, role: string) {
         await this.apiService.doPostRequest<void>(`/events/${event.uuid}/staff`, {name: name, role: role})
@@ -19,7 +17,7 @@ export class StaffService {
     }
 
     async getStaffs(event: IEvent): Promise<IStaff> {
-        return firstValueFrom(this.httpClient.get<IStaff>(`http://localhost:3000/api/events/${event.uuid}/staff`))
+        return this.apiService.doGetRequest<IStaff>(`/events/${event.uuid}/staff`)
     }
 
     async deleteStaff(event: IEvent, uuid: string) {
