2673b101f6ca015019a9ad668afd9da26a4a38f3
Jannis636 <32140646+Jannis636@users.noreply.github.com>
2023-04-14 12:05:00 +0200

    fix(#72): Make staff role lowercase in backend

--- src/app/modules/organizer/add-staff/manage-staff.component.ts
+++ src/app/modules/organizer/add-staff/manage-staff.component.ts
@@ -5,63 +5,69 @@ import {StaffService} from "../../../core/services/staff.service";
 import {IStaffItem} from "../../../models/IStaff";
 
 @Component({
-  selector: 'app-create-new-event',
-  templateUrl: './manage-staff.component.html'
+    selector: 'app-create-new-event',
+    templateUrl: './manage-staff.component.html'
 })
 
 
 export class ManageStaff implements OnInit {
 
-  constructor(private staffService: StaffService, private eventService: EventService) { }
+    constructor(private staffService: StaffService, private eventService: EventService) {
+    }
 
-  editStaffUUID: string | undefined;
+    editStaffUUID: string | undefined;
 
-  createStaffFormGroup = new FormGroup({
-    name: new FormControl<string | null>(null, {validators: [Validators.required]}),
-    role: new FormControl("Waiter")
-  });
+    createStaffFormGroup = new FormGroup({
+        name: new FormControl<string | null>(null, {validators: [Validators.required]}),
+        role: new FormControl("Waiter")
+    });
 
-  editStaffFormGroup = new FormGroup({
-    name: new FormControl<string | null>(null, {validators: [Validators.required]}),
-    role: new FormControl("Waiter")
-  });
+    editStaffFormGroup = new FormGroup({
+        name: new FormControl<string | null>(null, {validators: [Validators.required]}),
+        role: new FormControl("Waiter")
+    });
 
-  staffList = Array<IStaffItem>();
+    staffList = Array<IStaffItem>();
 
-  async ngOnInit() {
-    await this.updateStaffList();
-  }
+    async ngOnInit() {
+        await this.updateStaffList();
+    }
 
-  async addStaff() {
-    const event = await this.eventService.getEvent();
-    await this.staffService.addStaff(event, this.createStaffFormGroup.controls.name.value!,  this.createStaffFormGroup.controls.role.value!.toLowerCase())
-    this.createStaffFormGroup.controls.name.setValue("");
-    this.createStaffFormGroup.controls.role.setValue("Waiter");
-    await this.updateStaffList();
-  }
+    async addStaff() {
+        const event = await this.eventService.getEvent();
+        await this.staffService.addStaff(event, this.createStaffFormGroup.controls.name.value!, this.createStaffFormGroup.controls.role.value!.toLowerCase())
+        this.createStaffFormGroup.controls.name.setValue("");
+        this.createStaffFormGroup.controls.role.setValue("Waiter");
+        await this.updateStaffList();
+    }
 
-  async editStaffSetup(staffItem: IStaffItem) {
-    this.editStaffUUID = staffItem.uuid;
-    this.editStaffFormGroup.controls.name.setValue(staffItem.name)
-    this.editStaffFormGroup.controls.role.setValue(staffItem.role)
-  }
+    async editStaffSetup(staffItem: IStaffItem) {
+        this.editStaffUUID = staffItem.uuid;
+        this.editStaffFormGroup.controls.name.setValue(staffItem.name);
+        this.editStaffFormGroup.controls.role.setValue(this.staffRoleCapitalized(staffItem));
+    }
 
-  async editStaff() {
-    const event = await this.eventService.getEvent();
-    const uuid = this.editStaffUUID;
-    if(!uuid) return;
-    await this.staffService.editStaff(event, uuid, this.editStaffFormGroup.controls.name.value!,  this.editStaffFormGroup.controls.role.value!)
-    this.editStaffUUID = undefined;
-    await this.updateStaffList();
-  }
+    async editStaff() {
+        const event = await this.eventService.getEvent();
+        const uuid = this.editStaffUUID;
+        if (!uuid) return;
+        await this.staffService.editStaff(event, uuid, this.editStaffFormGroup.controls.name.value!, this.editStaffFormGroup.controls.role.value!);
+        this.editStaffUUID = undefined;
+        await this.updateStaffList();
+    }
 
-  async deleteStaff(uuid: string) {
-    await this.staffService.deleteStaff(await this.eventService.getEvent(), uuid);
-    await this.updateStaffList();
-  }
+    async deleteStaff(uuid: string) {
+        await this.staffService.deleteStaff(await this.eventService.getEvent(), uuid);
+        await this.updateStaffList();
+    }
 
-  async updateStaffList() {
-    const staff = await this.staffService.getStaffs(await this.eventService.getEvent())
-    this.staffList = staff.staffList;
-  }
+    async updateStaffList() {
+        const staff = await this.staffService.getStaffs(await this.eventService.getEvent());
+        this.staffList = staff.staffList;
+    }
+
+    staffRoleCapitalized(staffItem: IStaffItem): string {
+        const role = staffItem.role.toLowerCase();
+        return role[0].toUpperCase() + role.slice(1);
+    }
 }
