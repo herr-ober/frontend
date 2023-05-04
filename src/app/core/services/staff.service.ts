8338da5641de195fc7cbe297bb9cd868994e3682
Jannis636 <32140646+Jannis636@users.noreply.github.com>
2023-05-04 08:55:07 +0200

    improvement(#115): Add comments and favicon

--- src/app/core/services/staff.service.ts
+++ src/app/core/services/staff.service.ts
@@ -9,18 +9,22 @@ export class StaffService {
     constructor(private apiService: ApiService) {
     }
 
+    // Calls the server endpoint to add a new staff
     async addStaff(event: IEvent, name: string, role: string) {
         await this.apiService.doPostRequest<void>(`/events/${event.uuid}/staff`, {name: name, role: role})
     }
 
+    // Calls the server endpoint to edit a staff member
     async editStaff(event: IEvent, uuid: string, name: string, role: string) {
         await this.apiService.doPatchRequest<void>(`/events/staff`, {uuid: uuid, updates: {name: name, role: role}})
     }
 
+    // Calls the server endpoint to get all staff mebers
     async getStaffs(event: IEvent): Promise<IStaff> {
         return this.apiService.doGetRequest<IStaff>(`/events/${event.uuid}/staff`)
     }
 
+    // Calls the server endpoint to delete a staff member
     async deleteStaff(event: IEvent, uuid: string) {
         await this.apiService.doDeleteRequest<void>(`/events/staff`, {uuid: uuid})
     }
